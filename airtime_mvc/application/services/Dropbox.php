d1a859612d571b4be795c9c96d7d43a229542046
Eric Pugh <pugh.eric@gmail.com>
2013-09-17 15:24:10 -0400

    readded dbxClient to Dropbox

--- airtime_mvc/application/services/Dropbox.php
+++ airtime_mvc/application/services/Dropbox.php
@@ -10,13 +10,15 @@ use \Dropbox as dbx;
 
 class Application_Service_Dropbox
 {
+    private $_appInfo;
+    private $_webAuth;
     private $_dbxClient;
 
     private $_enabled;
     private $_authcode;
     private $_accesstoken;
-    private $_clientIdentifier = "airtime";
-    private $_appInfo;
+    private $_userid;
+    private $_clientIdentifier = "Airtime";
 
     public function __construct()
     {
@@ -25,17 +27,32 @@ class Application_Service_Dropbox
 
         $this->_enabled = Application_Model_Preference::GetEnableDropbox();
         $this->_authcode = Application_Model_Preference::GetDropboxAuthCode();
+        $this->_accesstoken = Application_Model_Preference::GetDropboxAccessToken();
+        $this->_webAuth = new dbx\WebAuthNoRedirect($this->_appInfo, $this->_clientIdentifier);
 
-        $this->_dbxClient = new dbx\Client($this->_accessToken, $this->_clientIdentifier); 
-
-
+        if($this->_accesstoken){
+            $this->_dbxClient = new dbx\Client($this->_accessToken, $this->_clientIdentifier);         
+        }
+        
     }
 
     public function getAuthorizationURL()
     {
-        //$webAuth = new dbx\WebAuthNoRedirect($this->_appInfo, $this->_clientIdentifier);
-        //return $webAuth->start();
-        return "this is url from the service";
+        return $this->_webAuth->start();
     }
+
+    public function createAccessToken()
+    {
+        if($this->_authcode){
+            list($this->_accesstoken, $this->_userid) = $this->_webAuth->finish($this->_authcode);
+            return $this->_accesstoken;
+        }
+    }
+
+    public function getAccessToken()
+    {
+        return $this->_accesstoken;
+    }
+    
 
 }
